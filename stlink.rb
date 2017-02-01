8505b47ec1c215b6df759fffab17fb865054b7ad
Marc-Etienne M.Léveillé <marc.etienne.ml@gmail.com>
2017-02-01 14:53:11 -0500

    Updated all hashes to SHA-256

--- stlink.rb
+++ stlink.rb
@@ -4,7 +4,7 @@ class Stlink < Formula
   version '1.0.0'
   homepage 'https://github.com/texane/stlink'
   url 'https://github.com/texane/stlink/archive/1.0.0.tar.gz'
-  sha1 'd55bbdd8c4c907be15b28d089fddc86e7a167766'
+  sha256 'd80af441be2c2be8a8e431e4ff6608f7c5ae52017c590d1c04b28bf0af0a3ddb'
 
   head do
     url 'https://github.com/texane/stlink.git'
